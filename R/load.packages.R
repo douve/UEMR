626147296f380de5a7a92823f04b547327bd0008
Oriol Prat Vallverdú <oprat@FJGOL.LOC>
2018-10-24 13:08:49 +0200

    removing last version.

--- R/load.packages.R
+++ R/load.packages.R
@@ -37,24 +37,6 @@
 
 
 
-
-load.packages <- function(x=NULL, update=F, dependent_pcs=T) {
-  default.pcs <- c("data.table","tidyverse","tableone",
-                   "anytime","VennDiagram","lubridate","epitools", "survival",
-                   "readxl")
-
-  if (dependent_pcs) x <- c(x,default.pcs)
-
-  boolps <- .info.packages(x)
-
-  if (sum(!boolps[[1]])) warning(boolps[[2]])
-  if (sum(boolps[[1]])) x = x[boolps[[1]]] # packages available to update or install
-
-  .prepare.packages(x,update)
-
-  invisible(lapply(x, require, character.only = TRUE))
-}
-
 load.packages <- function(x=NULL, update=F, dependent_pcs=T) {
   default.pcs <- c("data.table","tidyverse","tableone",
                    "anytime","VennDiagram","lubridate","epitools", "survival",
