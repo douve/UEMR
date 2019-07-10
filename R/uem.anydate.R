81e67bb0944b91ec0ea063bd310abbd356996a39
Oriol Prat Vallverdú <oprat@FJGOL.LOC>
2019-07-10 14:17:23 +0200

    millorant la conversió de les dates

--- R/uem.anydate.R
+++ R/uem.anydate.R
@@ -2,7 +2,7 @@
 uem.anydate = function(x){
   slength = stringr::str_length(x)
   iY = isYear(x)
-  if(Mode(slength)==8 & iY) x = anydate(as.character(x))
-  if(Mode(slength)==6 & iY) x = x %>% paste0(.,"01") %>% anydate
+  if(Mode(slength)==8 & iY) x = lubridate::ymd(as.character(x))
+  if(Mode(slength)==6 & iY) x = lubridate::ymd(as.character(paste0(x,"01")))
   return(x)
 }
