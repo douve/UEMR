b0371888cef93f52a655520262bc1d863bed7fec
douve <douchi@idiapjgol.info>
2019-08-02 11:08:43 +0200

    update isDate() function

--- R/isDate.R
+++ R/isDate.R
@@ -1,3 +1,4 @@
 isDate = function(x){
-  stringr::str_extract(x,'[[:digit:]][[:digit:]]') %in% c("19","20") %>% all(na.rm=T)
+  stringr::str_extract(na.omit(x),'[[:digit:]][[:digit:]]') %in% c("19","20") %>% all()
 }
+
