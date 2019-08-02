72934844243c7035aa535967d8fbb35304d60856
douve <douchi@idiapjgol.info>
2019-08-02 11:26:10 +0200

    uem anydate update

--- R/uem.anydate.R
+++ R/uem.anydate.R
@@ -2,7 +2,7 @@
 uem.anydate = function(x){
   slength = stringr::str_length(x)
   iY = isDate(x)
-  if(Mode(slength)==8 & iY) x = lubridate::ymd(as.character(x))
-  if(Mode(slength)==6 & iY) x = lubridate::ymd(as.character(paste0(x,"01")))
+  if(Mode(slength,na.rm=T)==8 & iY) x = lubridate::ymd(as.character(x))
+  if(Mode(slength,na.rm=T)==6 & iY) x = lubridate::ymd(as.character(paste0(x,"01")))
   return(x)
 }
