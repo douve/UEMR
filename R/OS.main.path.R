a7270e9890f02c1b8090ed027208ca70c76a9610
Dan Ouchi <douchi@irsi-sc-m110.local>
2020-03-12 16:22:36 +0100

    nn

--- R/OS.main.path.R
+++ R/OS.main.path.R
@@ -1,9 +1,9 @@
 
 
-OS.main.path = function(OS=get_os(),oneDrive = F){
-  if(OS=='osx' & !oneDrive) path = '/Users/user/Documents/' # Mac Dan
-  if(OS=='osx' & oneDrive) path = '/Users/user/OneDrive - Instituto de Investigación del Sida IrsiCaixa/' # Mac Dan
+OS.main.path = function(OS=get_os(),user='user',oneDrive = F){
+  if(OS=='osx' & !oneDrive) path = paste0('/Users/',user,'/Documents/') # Mac Dan
+  if(OS=='osx' & oneDrive) path = paste0('/Users/',user,'/OneDrive - Instituto de Investigación del Sida IrsiCaixa/') # Mac Dan
   if(OS=='windows') path = 'Z:/' # PC Oriol
-  if(OS=='linux') path = '/run/user/1000/gvfs/smb-share:server=epofs.fjgol.loc,share=douchi/' # Unix IDIAP
+  if(OS=='linux') path = paste0('/run/',user,'/1000/gvfs/smb-share:server=epofs.fjgol.loc,share=douchi/') # Unix IDIAP
   return(path)
 }
