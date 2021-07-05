f03dbdf1f2e8e11842f61637cdffac0048c33570
Dan  Ouchi <danouchi@Dans-MacBook-Air.local>
2021-07-05 14:37:40 +0200

    update path new mac

--- R/OS.main.path.R
+++ R/OS.main.path.R
@@ -1,8 +1,8 @@
 
 
-OS.main.path = function(OS=get_os(),user='user',oneDrive = F){
-  if(OS=='osx' & !oneDrive) path = paste0('/Users/',user,'/Documents/') # Mac Dan
-  if(OS=='osx' & oneDrive) path = paste0('/Users/',user,'/OneDrive - Instituto de Investigación del Sida IrsiCaixa/') # Mac Dan
+OS.main.path = function(OS=get_os(),user='danouchi',cloud = F){
+  if(OS=='osx' & !cloud) path = paste0('/Users/',user,'/Documents/') # Mac Dan
+  if(OS=='osx' & cloud) path = paste0('/Users/',user,'/Google Drive/My Drive/') #Drive Dan
   if(OS=='windows') path = 'Z:/' # PC Oriol
   if(OS=='linux') path = paste0('/run/',user,'/1000/gvfs/smb-share:server=epofs.fjgol.loc,share=douchi/') # Unix IDIAP
   return(path)
